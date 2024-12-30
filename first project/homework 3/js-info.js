91cb607021c1a2142387456a21af1183da09a451
Ivan <karez79@yandex.ru>
2024-12-30 19:24:39 +0300

    Задачи с 1-4

---
+++ first project/homework 3/js-info.js
@@ -0,0 +1,5 @@
+console.log('Кто разработал язык программирования JavaScript?', 'ОТВЕТ: Брэндан Эйх');
+console.log('За сколько дней был разработан JavaScript?', 'ОТВЕТ: За 10 дней');
+console.log('В каком году появился JavaScript?', 'ОТВЕТ: В 1995 году.');
+console.log('JavaScript — это то же самое, что и Java?', 'ОТВЕТ: JavaScript — это не Java');
+console.log('Как называется спецификация JavaScript?', 'ОТВЕТ: ECMAScript');
