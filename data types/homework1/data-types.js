da991d93287d05a254789a9e6bef0bae89b7958a
Ivan <karez79@yandex.ru>
2025-01-01 21:29:26 +0300

    data types

---
+++ data types/homework1/data-types.js
@@ -0,0 +1,25 @@
+const name = 'Ivan'
+const age  = 29
+const kachok = false
+const dog = null
+const movie = undefined
+const all = {
+    name: 'Petr',
+    age: 29,
+    kachok: false,
+    dog: '',
+}
+
+const bigNumber = 100n
+const id = Symbol('id')
+
+
+
+console.log('name', name, typeof name)
+console.log('age', age, typeof age)
+console.log('kachok', kachok, typeof kachok)
+console.log('dog', dog, typeof dog)
+console.log('movie', movie, typeof movie)
+console.log('all', all, typeof all)
+console.log('bigNumber', bigNumber, typeof bigNumber)
+console.log('id', id, typeof id)
---
+++ data types/homework2/index.js
@@ -0,0 +1,12 @@
+let name = 'Ivan'
+let age  = 29
+let kachok = false
+
+
+age = 'ivan' //строка
+name = 29 // число
+kachok = null //null
+
+console.log('name', name, typeof name)
+console.log('age', age, typeof age)
+console.log('kachok', kachok, typeof kachok)
