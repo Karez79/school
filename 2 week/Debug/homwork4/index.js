70f4220dbb78fa2ac1035cb29cb79e137e9f8815
Ivan <karez79@yandex.ru>
2025-01-13 22:56:42 +0300

    fix: debug 4

--- 2 week/Debug/homwork4/index.js
+++ 2 week/Debug/homwork4/index.js
@@ -1,19 +1,17 @@
-let numbers = [10, 4, 100, -5, 54, 2]
+let numbers = [10, 4, 100, -5, 54, 2];
 let sum = 0;
 
 // Через цикл for
 for (let i = 0; i < numbers.length; i += 1) {
-    numbers[i] = numbers[i] ** 3;
-    sum += numbers[i];
-} 
+    let another = numbers[i] ** 3;  // Возводим в куб без изменения массива
+    sum += another;
+}
 console.log(sum); // 1158411
 
 // Через цикл for of
-sum = 0;
-numbers = [10, 4, 100, -5, 54, 2]
-console.log(numbers)
-for (let num of numbers){
-    num = num ** 3;
-    sum += num;
-} 
-console.log('2', sum); // 1003904306910622800
+sum = 0;  // Обнуляем сумму
+for (let num of numbers) {
+    let another = num ** 3;  // Возводим число в куб
+    sum += another;  // Добавляем к общей сумме
+}
+console.log('2', sum); // 1158411
